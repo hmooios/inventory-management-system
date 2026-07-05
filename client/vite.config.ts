00e10eb62bf928718473a7dc51dd15add9961ae1
htetzawphyo <htetzawphyo.hpn@gmail.com>
2026-07-05 14:02:41 +0630

    fix vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const allowedHosts = [
+  'inventoryadmin.htetzawphyo.dev',
+  'inventoryclient.htetzawphyo.dev'
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts
+  }
 });
--- customer-client/vite.config.ts
+++ customer-client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const allowedHosts = [
+  'inventoryadmin.htetzawphyo.dev',
+  'inventoryclient.htetzawphyo.dev'
+];
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5174
+    port: 5174,
+    allowedHosts
   }
 });
