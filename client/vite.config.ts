625cbbd35582b94bb266003df285ae0bb5865b47
htetzawphyo <htetzawphyo.hpn@gmail.com>
2026-07-09 10:46:51 +0630

    add old domain

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const allowedHosts = [
-  'inventory-system.store',
-  'client.inventory-system.store'
+  "inventory-system.store",
+  "client.inventory-system.store",
+  "inventoryadmin.htetzawphyo.dev",
+  "inventoryclient.htetzawphyo.dev",
 ];
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    allowedHosts
-  }
+    host: "0.0.0.0",
+    allowedHosts,
+  },
 });
--- customer-client/vite.config.ts
+++ customer-client/vite.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const allowedHosts = [
-  'inventory-system.store',
-  'client.inventory-system.store'
+  "inventory-system.store",
+  "client.inventory-system.store",
+  "inventoryadmin.htetzawphyo.dev",
+  "inventoryclient.htetzawphyo.dev",
 ];
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5174,
-    allowedHosts
-  }
+    allowedHosts,
+  },
 });
